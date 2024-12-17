7d4359923bcb966886d3c113b7f791cbee55cd96
Net test <hryst2092@mail.ru>
2024-12-17 21:33:57 +0300

    Changed FreelancerService.java

--- src/main/java/ru/netology/services/FreelancerService.java
+++ src/main/java/ru/netology/services/FreelancerService.java
@@ -3,19 +3,16 @@ package ru.netology.services;
 public class FreelancerService {
 
     public int calculateRestMonths(int income, int expenses, int threshold) {
-        int restMonths = 0;
-        int savings = 0;
-
-        for (int month = 1; month <= 12; month++) {
-            if (savings >= threshold) {
-                restMonths++;
-                savings -= expenses;
-                savings -= savings / 3;
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
+        for (int month = 0; month < 12; month++) {
+            if (money >= expenses) { // можем ли отдыхать?
+                count++; // увеличиваем счётчик месяцев отдыха
+                money = money - expenses;
             } else {
-                savings += income;
-                savings -= expenses;
+                money = money + income;
             }
         }
-        return restMonths;
+        return count;
     }
 }
